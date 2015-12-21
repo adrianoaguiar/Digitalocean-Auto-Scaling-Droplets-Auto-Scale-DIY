897ad4c0e6091d1545cb65a3bf1fb7856d2fd8b6
magenx <magenx@users.noreply.github.com>
2015-12-21 17:37:57 +0000

    Update create_droplet.sh

--- create_droplet.sh
+++ create_droplet.sh
@@ -9,10 +9,10 @@ SSH="xxxxxx"
 # Get CPU qty
 CPU=$(cat /proc/cpuinfo | grep "^processor" | wc -l)
 # Calculate droplet average load 5min
-LOAD_5=$(cat /proc/loadavg | awk '{print $1}')
-LOAD_AVERAGE_5=$(($(echo ${LOAD_5} | awk '{print 100 * $1}') / ${CPU}))
+LOAD_1=$(cat /proc/loadavg | awk '{print $1}')
+LOAD_AVERAGE_1=$(($(echo ${LOAD_1} | awk '{print 100 * $1}') / ${CPU}))
 # Droplet average 5min load > 75% then create new droplet
-if [ ${LOAD_AVERAGE_5} -ge 75 ] ; then
+if [ ${LOAD_AVERAGE_1} -ge 75 ] ; then
 curl -s -X POST "${API_URL}/droplets" \
   -d"{\"name\":\"magento-web-node-${NODE_ID}\",\"region\":\"nyc1\",\"size\":\"2gb\",\"private_networking\":true,\"image\":\"${IMAGE_ID}\",\"ssh_keys\":[${SSH}]}" \
 	-H "Authorization: Bearer ${TOKEN}" \
